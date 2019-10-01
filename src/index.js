05f1667b3e67ad31c313e9aaddfe791d6eced221
Joe Gerhard <jlgerhard@gmail.com>
2019-09-30 22:58:20 -0500

    Add styling to SearchPage

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './fonts/AvenirNextLTPro-Regular.otf'
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
