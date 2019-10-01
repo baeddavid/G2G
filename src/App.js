b21e63e10e562f757463292cdc51a711a4f3aee6
Joe Gerhard <jlgerhard@gmail.com>
2019-10-01 13:05:38 -0500

    Refactor to use react Hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar'
 import ViewScreen from './components/ViewScreen/ViewScreen'
 import './App.css';
 
 
-class App extends Component {
-  state = {
-    user: {
-      name: 'Ana'
-    }
-  }
-  render = () => 
+const App = () => {
+
+  const [user, setUser] = useState({
+    name: 'Ana'
+  })
+  
+  return ( 
     <div className="App">
-      <ViewScreen userName={this.state.user.name} />
+      <ViewScreen userName={user.name} />
       <Navbar />
     </div>
+  )
 }
 
 export default App;
