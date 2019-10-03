efaecc1a619370449aeb60d3ada00c0171fe60ce
Jack Ground <jaground@gmail.com>
2019-10-02 19:26:29 -0500

    adds cutom styled map to searchPage

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import styles from './SearchPage.module.css';
-import Map from '../../components/Map/Map';
+import WrappedMap from '../../components/Map/Map';
+
+const googleMapsURL = `https://maps.googleapis.com/maps/api/js?&key=`
+const googleAPIKey =  'AIzaSyA7X9opLTmxQJT4SEvtea90bXDqm7OUZ2s'   //'AIzaSyAkFzpnSdVoZTo-KSm-i4VvQWNWAPe5gDc'
+
 
 const SearchPage = (props) => 
   <div className={styles.SearchPage}>
-    <Map />
+    <div className="map-wrapper" style={{height: "100vh", width: "100vw"}}>
+      <WrappedMap
+        googleMapURL={`${googleMapsURL + googleAPIKey}`}
+        loadingElement={ <div style={{ height: '100%'}}/> }
+        containerElement={ <div className='container' style={{ height: '100%'}}/> }
+        mapElement={ <div className='map' style={{ height: '100%'}}/> }
+      />
+    </div>
     <div className={styles.inputArea}>
       <div className={styles.inputGroup}>
         <div className={styles.addBathroomButton}>+</div>
