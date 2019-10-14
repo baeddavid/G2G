415319c33e2022d9b3089d9defce38ecc4b34cea
Joe Gerhard <jlgerhard@gmail.com>
2019-10-13 23:00:10 -0500

    Add destructuring syntax for Button styled-components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 
-const StyledButton = styled.div`
+const StyledButton = styled.div(({theme, ...props}) => css`
   align-items: center;
-  background: ${({primary, theme}) => primary ? theme.primaryDark : theme.primary};
-  border: .5vh solid ${({theme}) => theme.primaryDark};
+  background: ${props.primary ? theme.primaryDark : theme.primary};
+  border: .5vh solid ${theme.primaryDark};
   border-radius: 5vh;
   color: white;
   display: flex;
@@ -16,11 +15,11 @@ const StyledButton = styled.div`
   justify-content: center;
   line-height: 33px;
   margin: 0 auto;
-  margin-bottom: ${props => props.marginBottom || '10vh'};
-  margin-top: ${props => props.marginTop || 'auto'};
+  margin-bottom: ${props.marginBottom || '10vh'};
+  margin-top: ${props.marginTop || 'auto'};
   transition: background-color .5s;
   width: 68vw;
-`;
+`);
 
 const Button = ({ children, onClick, primary, disabled, ...props }) => {
   
